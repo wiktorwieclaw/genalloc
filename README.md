bdf4e5e59428ede9a147aa2ecb9a63d426a3f3d6
Vixu <lonevixu@gmail.com>
2024-03-29 15:59:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Generational Allocations
+Heavily inspired by [generational-box](https://crates.io/crates/generational-box).
+
+## Examples
+```rust
+let span = genalloc::Span::new();
+let ptr: genalloc::Ptr<u32> = span.alloc(5); // No lifetimes! `Ptr` is `Copy`!
+assert_eq!(*ptr.read(), 5);
+```
+
+`Span` is the owner of the memory so the `Ptr` is valid as long as it's `Span` is valid.
+```rust
+let ptr = {
+    let mut span = genalloc::Span::new();
+    span.alloc(5)
+};
+*ptr.read(); // Panics!
+```
---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+#![doc = include_str!("../README.md")]
+
+use std::{
+    any::Any,
+    cell::{Ref, RefCell, RefMut},
+    marker::PhantomData,
+};
+
+/// Generational allocations span.
+#[derive(Default)]
+pub struct Span(Vec<Alloc>);
+
+impl Span {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Allocates `v` on the heap and stores the pointer in a generational allocation.
+    ///
+    /// Then the [`Span`] gets dropped it recycles it's generational allocations so that they
+    /// can be reused by other spans.
+    /// 
+    /// The returned [`Ptr<T>`] is [`Copy`] even if the underlying `T` is not [`Copy`].
+    /// This pointer gets invalidated whenever it's [`Span`] is dropped.
+    #[must_use]
+    pub fn alloc<T: 'static>(&mut self, v: T) -> Ptr<T> {
+        let alloc = RECYCLED_ALLOCS
+            .with(|recycled| recycled.borrow_mut().pop())
+            .unwrap_or_default();
+        self.0.push(alloc);
+        *alloc.ptr.borrow_mut() = Some(Box::new(v));
+        Ptr {
+            gen: alloc.gen,
+            alloc,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl Drop for Span {
+    fn drop(&mut self) {
+        RECYCLED_ALLOCS.with(|recycled| {
+            let mut recycled = recycled.borrow_mut();
+            for alloc in &mut self.0 {
+                let _ = alloc.ptr.take();
+                alloc.gen += 1;
+                recycled.push(*alloc)
+            }
+        });
+    }
+}
+
+/// Generational pointer.
+/// 
+/// [`Ptr<T>`] is [`Copy`] even if the underlying `T` is not [`Copy`].
+#[derive(Clone, Copy)]
+pub struct Ptr<T> {
+    alloc: Alloc,
+    gen: u32,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Ptr<T> {
+    pub fn read(&self) -> Ref<'static, T> {
+        assert_eq!(self.gen, self.alloc.gen);
+        let borrow = self.alloc.ptr.borrow();
+        Ref::filter_map(borrow, |any| any.as_ref()?.downcast_ref()).unwrap()
+    }
+
+    pub fn write(&self) -> RefMut<'static, T> {
+        assert_eq!(self.gen, self.alloc.gen);
+        let borrow = self.alloc.ptr.borrow_mut();
+        RefMut::filter_map(borrow, |any| any.as_mut()?.downcast_mut()).unwrap()
+    }
+}
+
+/// Generational allocation.
+#[derive(Clone, Copy)]
+struct Alloc {
+    ptr: &'static RefCell<Option<Box<dyn Any>>>,
+    gen: u32,
+}
+
+impl Default for Alloc {
+    fn default() -> Self {
+        Self {
+            ptr: &*Box::leak(Default::default()),
+            gen: 0,
+        }
+    }
+}
+
+thread_local! {
+    static RECYCLED_ALLOCS: RefCell<Vec<Alloc>> = const {
+        RefCell::new(Vec::new())
+    };
+}
